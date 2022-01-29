3fe871689bbd98b6f02fe18b1344274d5be9444f
0left <leftamail@gmail.com>
2022-01-29 00:36:49 -0300

    Adicionei o Pulin
    ainda vou ajustar distancai e a fisica melhorzin com a fase, mas o pulin ta legal até, gostei

---
+++ Assets/GroundRec.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundRec : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Ground")){
+            gameObject.transform.parent.transform.gameObject.GetComponent<MainChar_Mov>().isOnGround();
+        }
+    }
+}
--- Assets/MainChar_Mov.cs
+++ Assets/MainChar_Mov.cs
@@ -16,13 +16,42 @@ public class MainChar_Mov : MonoBehaviour
         //rb.AddForce(move * speed * Time.deltaTime, ForceMode2D.Force);
         //rb.velocity = move * speed * Time.deltaTime;
         //rb.MovePosition(rb.position + (move * speed * Time.deltaTime));
-        rb.velocity = new Vector2(move.x * speed * Time.deltaTime, rb.velocity.y);
+        if(StopAndChange){
+            StopAndChange = false;
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+        if(rb.velocity.x > -101 && rb.velocity.x < 101){
+            Debug.Log(rb.velocity.x);
+            if(canJump){
+                rb.velocity = new Vector2(move.x * speed * Time.deltaTime, rb.velocity.y);
+            }else{
+                rb.velocity += new Vector2(move.x * (speed / 100) *Time.deltaTime, 0);
+            }
+        }
     }
-    private bool Jump;
-
+    private bool GoingToRight;
+    private bool StopAndChange;
+    public bool canJump = true;
+    public float jumpVelocity = 300;
+    public void isOnGround(){
+        canJump = true;
+        return;
+    }
     void Update()
     {
+        if(Input.GetAxisRaw("Horizontal") < 0 && GoingToRight){
+            GoingToRight = false;
+            GetComponent<SpriteRenderer>().flipX = true;
+            StopAndChange = true;
+        }else if (Input.GetAxisRaw("Horizontal") > 0 && !GoingToRight){
+            GoingToRight = true;
+            GetComponent<SpriteRenderer>().flipX = false;
+            StopAndChange = true;
+        }
         move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        //if (Input.GetKeyDown("space")){Jump = true;}else{Jump = false;}
+        if (Input.GetKeyDown("space") && canJump){
+            canJump = false;
+            rb.AddForce(Vector2.up*jumpVelocity, ForceMode2D.Impulse);
+        }
     }
 }
