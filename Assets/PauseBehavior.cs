b65ba5813f30baf6eb4a7e7e34b14c7060bf5913
0left <leftamail@gmail.com>
2022-01-30 13:22:36 -0300

    Pause finalizado

--- Assets/PauseBehavior.cs
+++ Assets/PauseBehavior.cs
@@ -1,19 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    bool isPaused;
+    public GameObject PauseMenu;
     // Update is called once per frame
     void Update()
     {
-        if()
-        
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(isPaused){
+                PauseMenu.SetActive(false);
+                Time.timeScale = 1f;
+                isPaused = false;
+            }else{
+                PauseMenu.SetActive(true);
+                Time.timeScale = 0f;
+                isPaused = true;
+
+            }
+        }
+    }
+    public void RestartLevel(){
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void BackToMenu(){
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main_Menu");
+    }
+    public void QuitGame(){
+        Application.Quit();
     }
 }
